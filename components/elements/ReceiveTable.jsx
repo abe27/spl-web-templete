999ff2c96a7ddd2bb29bc321d92cc6344752828c
Taweechai Yuenyang <krumii.it@gmail.com>
2023-01-26 16:43:30 +0700

    show receive page

--- components/elements/ReceiveTable.jsx
+++ components/elements/ReceiveTable.jsx
@@ -90,7 +90,7 @@ const ReceiveTable = ({ limit = 15 }) => {
 
   useEffect(() => {
     FetchData();
-  }, []);
+  }, [limit]);
 
   return (
     <>
@@ -103,194 +103,26 @@ const ReceiveTable = ({ limit = 15 }) => {
       <div class="overflow-x-auto">
         <div className="flex flex-col lg:flex-row justify-between items-start lg:items-stretch w-full">
           <div className="w-full lg:w-1/3 flex flex-col lg:flex-row items-start lg:items-center">
-            <div className="flex items-center">
-              <a
-                className="text-gray-600 dark:text-gray-400 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray"
-                href="javascript: void(0)"
-              >
+            <div className="flex items-center space-x-4">
+              <div className="btn btn-ghost btn-sm" onClick={() => FetchData()}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="icon cursor-pointer icon-tabler icon-tabler-edit"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
                   fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                  <line x1={16} y1={5} x2={19} y2={8} />
-                </svg>
-              </a>
-              <a
-                className="text-gray-600 dark:text-gray-400 mx-2 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray"
-                href="javascript: void(0)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon cursor-pointer icon-tabler icon-tabler-settings"
-                  width={20}
-                  height={20}
                   viewBox="0 0 24 24"
-                  strokeWidth="1.5"
+                  strokeWidth={1.5}
                   stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  className="w-6 h-6"
                 >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                  <circle cx={12} cy={12} r={3} />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
                 </svg>
-              </a>
-              <a
-                className="text-gray-600 dark:text-gray-400 mr-2 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray"
-                href="javascript: void(0)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-bookmark"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2" />
-                </svg>
-              </a>
-              <a
-                className="text-gray-600 dark:text-gray-400 mr-2 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray"
-                href="javascript: void(0)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-copy"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <rect x={8} y={8} width={12} height={12} rx={2} />
-                  <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2" />
-                </svg>
-              </a>
-              <a
-                className="text-red-500 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray"
-                href="javascript: void(0)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon cursor-pointer icon-tabler icon-tabler-trash"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <line x1={4} y1={7} x2={20} y2={7} />
-                  <line x1={10} y1={11} x2={10} y2={17} />
-                  <line x1={14} y1={11} x2={14} y2={17} />
-                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-              </a>
+              </div>
             </div>
           </div>
           <div className="w-full lg:w-2/3 flex flex-col lg:flex-row items-start lg:items-center justify-end">
-            <div className="flex items-center lg:border-l lg:border-r border-gray-300 dark:border-gray-200 py-3 lg:py-0 lg:px-6">
-              <p
-                className="text-base text-gray-600 dark:text-gray-400"
-                id="page-view"
-              >
-                Viewing 1 - 20 of 60
-              </p>
-              <a
-                className="text-gray-600 dark:text-gray-400 ml-2 border-transparent border cursor-pointer rounded"
-                onclick="pageView(false)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-chevron-left"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <polyline points="15 6 9 12 15 18" />
-                </svg>
-              </a>
-              <a
-                className="text-gray-600 dark:text-gray-400 border-transparent border rounded focus:outline-none cursor-pointer"
-                onclick="pageView(true)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-chevron-right"
-                  width={20}
-                  height={20}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <polyline points="9 6 15 12 9 18" />
-                </svg>
-              </a>
-            </div>
-            <div className="flex items-center lg:border-r border-gray-300 dark:border-gray-200 pb-3 lg:pb-0 lg:px-6">
-              <div className="relative w-32 z-10">
-                <div className="pointer-events-none text-gray-600 dark:text-gray-400 absolute inset-0 m-auto mr-2 xl:mr-4 z-0 w-5 h-5">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon cursor-pointer icon-tabler icon-tabler-chevron-down"
-                    width={20}
-                    height={20}
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" />
-                    <polyline points="6 9 12 15 18 9" />
-                  </svg>
-                </div>
-                <select
-                  aria-label="Selected tab"
-                  className="focus:outline-none border border-transparent focus:border-gray-800 focus:shadow-outline-gray text-base form-select block w-full py-2 px-2 xl:px-3 rounded text-gray-600 dark:text-gray-400 appearance-none bg-transparent"
-                >
-                  <option>List View</option>
-                  <option>Grid View</option>
-                </select>
-              </div>
-            </div>
             <div className="lg:ml-6 flex items-center">
               <button
                 onClick={handleUploadExcelClick}
--- pages/receive/index.jsx
+++ pages/receive/index.jsx
@@ -1,13 +1,16 @@
-import { MainLayOut, ReceiveTable } from "@/components"
+import { MainLayOut, ReceiveTable } from "@/components";
 
 const ReceivePage = () => {
   return (
-    <MainLayOut title={`รายการรับสินค้า`} description={`จัดการข้อมูลการรับสินค้า`}>
+    <MainLayOut
+      title={`รายการรับสินค้า`}
+      description={`จัดการข้อมูลการรับสินค้า`}
+    >
       <>
-      <ReceiveTable limit={20}/>
+        <ReceiveTable limit={15} />
       </>
     </MainLayOut>
-  )
-}
+  );
+};
 
-export default ReceivePage
+export default ReceivePage;
