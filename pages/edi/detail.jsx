e64155cfbd791d7c1b02f746afc3920304c7f061
Taweechai Yuenyang <krumii.it@gmail.com>
2023-01-30 15:54:26 +0700

    new bg

--- pages/edi/detail.jsx
+++ pages/edi/detail.jsx
@@ -2,16 +2,24 @@ import { MainLayOut } from "@/components";
 import { useEffect } from "react";
 
 const EdiDetailPage = () => {
-  useEffect(() => {
-
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <MainLayOut
       title={`แสดงข้อมูล EDI TEXT`}
       description={`จัดการข้อมูล EDI TEXT`}
     >
-      <h1>Receive Page</h1>
+      <div className="bg-gray-400 mockup-code">
+        <pre data-prefix="$">
+          <code>npm i daisyui</code>
+        </pre>
+        <pre data-prefix=">" className="text-warning">
+          <code>installing...</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          <code>Done!</code>
+        </pre>
+      </div>
     </MainLayOut>
   );
 };
